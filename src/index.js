e3aaefe2b61f2fbd97804d1844de7ecad9ed08bc
Peter de Verteuil <peter@500px.com>
2017-10-01 21:59:10 -0400

    added style and new routes

--- src/index.js
+++ src/index.js
@@ -2,19 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import './style.css';
 import Home from './components/home'
 import Panorama from './components/panorama'
 
 ReactDOM.render(
   <Router>
     <div>
+      <h1>The beauty of panoramic photography</h1>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/panorama">Park</Link></li>
+        <li><Link to="/boat">Boat</Link></li>
+        <li><Link to="/park">Park</Link></li>
       </ul>
 
       <Route exact path="/" component={Home} />
-      <Route path="/panorama" component={Panorama} />
+      <Route
+        path="/boat"
+        render={(props) => <Panorama {...props} imageURL="./boat.jpg" />}
+      />
+      <Route
+        path="/park"
+        render={(props) => <Panorama {...props} imageURL="./park.jpg" />}
+      />
     </div>
   </Router>
   ,
