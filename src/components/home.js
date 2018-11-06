6992ef5bb54ed3082f2b5e72981370399a35a657
PierreDV <p.verteuil@gmail.com>
2018-11-06 20:14:53 +0100

    image test

--- src/components/home.js
+++ src/components/home.js
@@ -4,6 +4,7 @@ export default class Home extends Component {
   render() {
     return (
       <div>
+        <img src="./sunset.JPG" ></img>
         <p>Click on a scene to see in panorama view</p>
       </div>
     );
