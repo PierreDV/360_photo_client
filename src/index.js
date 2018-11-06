c95bdbe2a0a1bb44072d7dfff4b58adb6a9103a8
PierreDV <p.verteuil@gmail.com>
2018-11-06 19:56:42 +0100

    change jps file reference to use uppercase

--- src/index.js
+++ src/index.js
@@ -23,23 +23,23 @@ ReactDOM.render(
       <Route exact path="/" component={Home} />
       <Route
         path="/dusk"
-        render={(props) => <Panorama {...props} imageURL="./dusk.jpg" />}
+        render={(props) => <Panorama {...props} imageURL="./dusk.JPG" />}
       />
       <Route
         path="/sunset"
-        render={(props) => <Panorama {...props} imageURL="./sunset.jpg" />}
+        render={(props) => <Panorama {...props} imageURL="./sunset.JPG" />}
       />
       <Route
         path="/forest"
-        render={(props) => <Panorama {...props} imageURL="./forest.jpg" />}
+        render={(props) => <Panorama {...props} imageURL="./forest.JPG" />}
       />
       <Route
         path="/pier"
-        render={(props) => <Panorama {...props} imageURL="./pier.jpg" />}
+        render={(props) => <Panorama {...props} imageURL="./pier.JPG" />}
       />
       <Route
         path="/lake"
-        render={(props) => <Panorama {...props} imageURL="./lake.jpg" />}
+        render={(props) => <Panorama {...props} imageURL="./lake.JPG" />}
       />
     </div>
   </Router>
